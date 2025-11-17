45fc590b2beb700db9d828ed189fb37ce56b8639
Apong516 <93095652+Apong516@users.noreply.github.com>
2025-11-17 23:49:32 +0800

    Add files via upload

---
+++ pages/secret.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqCqt9wXYr1lB2Rw7FewY-2Ipk6etc49Q",
+  authDomain: "taskdash-c5cc1.firebaseapp.com",
+  projectId: "taskdash-c5cc1",
+  storageBucket: "taskdash-c5cc1.firebasestorage.app",
+  messagingSenderId: "822323316343",
+  appId: "1:822323316343:web:8033f41600c040cf4771f6e",
+  measurementId: "G-8ZQ4R12DDF"
+};
